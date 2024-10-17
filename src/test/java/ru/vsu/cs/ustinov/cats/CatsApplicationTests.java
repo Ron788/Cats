81f9d8b5aa877379973b18725cc21eb6560997ed
Mikhail Ustinov <ustinov200511@gmail.com>
2024-10-17 18:20:23 +0300

    nikogda ne pozdno

---
+++ src/test/java/ru/vsu/cs/ustinov/cats/CatsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.ustinov.cats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
